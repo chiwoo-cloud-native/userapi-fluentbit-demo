a434b1c790ae2ab9811bf2ff7cf72098665570e4
symplesims <symplesims@gmail.com>
2022-12-08 01:23:20 +0900

    fluentbit 도커이미지 빌드 추가

--- cicd/fluentbit/docker/entrypoint.sh
+++ cicd/fluentbit/docker/entrypoint.sh
@@ -1,10 +1,10 @@
 echo -n "AWS for Fluent Bit Test Image Version "
 cat /AWS_FOR_FLUENT_BIT_VERSION
 
-# sed -i "s/TASK_ID/$TASK_ID/g" /fluent-bit/etc/fluent-bit.conf
-# sed -i "s/TASK_DEFINITION/$FAMILY:$REV/g" /fluent-bit/etc/fluent-bit.conf
-# sed -i "s/ECS_CLUSTER/$ECS_CLUSTER/g" /fluent-bit/etc/fluent-bit.conf
+sed -i "s#{aws_region}#$ECS_AWS_REGION#g" /fluent-bit/etc/fluent-bit.conf
+sed -i "s#{cluster_name}#$ECS_CLUSTER_NAME#g" /fluent-bit/etc/fluent-bit.conf
+sed -i "s#{app_name}#$ECS_APP_NAME#g" /fluent-bit/etc/fluent-bit.conf
+sed -i "s#{log_filepath}#$LOG_FILEPATH#g" /fluent-bit/etc/fluent-bit.conf
 
 # see - https://github.com/aws/aws-for-fluent-bit/blob/mainline/entrypoint.sh
 exec /fluent-bit/bin/fluent-bit -e /fluent-bit/firehose.so -e /fluent-bit/cloudwatch.so -e /fluent-bit/kinesis.so -c /fluent-bit/etc/fluent-bit.conf
-
