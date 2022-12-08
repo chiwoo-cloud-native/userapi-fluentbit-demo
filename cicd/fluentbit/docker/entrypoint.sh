e4daea3cc4c88310ff998e9f62479a96e80738af
symplesims <symplesims@gmail.com>
2022-12-08 10:25:29 +0900

    container 간 볼륨 공유를 위한 구성 추가

--- cicd/fluentbit/docker/entrypoint.sh
+++ cicd/fluentbit/docker/entrypoint.sh
@@ -6,5 +6,7 @@ sed -i "s#{cluster_name}#$ECS_CLUSTER_NAME#g" /fluent-bit/etc/fluent-bit.conf
 sed -i "s#{app_name}#$ECS_APP_NAME#g" /fluent-bit/etc/fluent-bit.conf
 sed -i "s#{log_filepath}#$LOG_FILEPATH#g" /fluent-bit/etc/fluent-bit.conf
 
+cat /fluent-bit/etc/fluent-bit.conf
+
 # see - https://github.com/aws/aws-for-fluent-bit/blob/mainline/entrypoint.sh
 exec /fluent-bit/bin/fluent-bit -e /fluent-bit/firehose.so -e /fluent-bit/cloudwatch.so -e /fluent-bit/kinesis.so -c /fluent-bit/etc/fluent-bit.conf
