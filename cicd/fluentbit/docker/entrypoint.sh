e6ae07d9fe0c4ed2aff2d2366b7ff05d53a7bdc9
symplesims <symplesims@gmail.com>
2022-12-08 18:02:30 +0900

    fluent-bit.conf 설정 완료

--- cicd/fluentbit/docker/entrypoint.sh
+++ cicd/fluentbit/docker/entrypoint.sh
@@ -6,8 +6,8 @@ sed -i "s#{cluster_name}#$ECS_CLUSTER_NAME#g" /fluent-bit/etc/fluent-bit.conf
 sed -i "s#{app_name}#$ECS_APP_NAME#g" /fluent-bit/etc/fluent-bit.conf
 sed -i "s#{log_filepath}#$LOG_FILEPATH#g" /fluent-bit/etc/fluent-bit.conf
 
-ls -al /var/log/myapp/
-cat /fluent-bit/etc/fluent-bit.conf
+# ls -al /var/log/myapp/
+# cat /fluent-bit/etc/fluent-bit.conf
 
 # see - https://github.com/aws/aws-for-fluent-bit/blob/mainline/entrypoint.sh
 exec /fluent-bit/bin/fluent-bit -e /fluent-bit/firehose.so -e /fluent-bit/cloudwatch.so -e /fluent-bit/kinesis.so -c /fluent-bit/etc/fluent-bit.conf
