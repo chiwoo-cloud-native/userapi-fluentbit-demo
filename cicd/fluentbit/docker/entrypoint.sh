2b7ff712f30b16f2f08c0a2fa10c3e5a3b4557d9
symplesims <symplesims@gmail.com>
2022-12-13 02:23:48 +0900

    entrypoint.sh 스크립트 로그 출력

--- cicd/fluentbit/docker/entrypoint.sh
+++ cicd/fluentbit/docker/entrypoint.sh
@@ -1,10 +1,36 @@
 echo -n "AWS for Fluent Bit Test Image Version "
 cat /AWS_FOR_FLUENT_BIT_VERSION
 
-echo "ecs_task_id: $(ecs_task_id) , ecs_cluster: $(ecs_cluster)"
-echo "ecs_task_id2: $ecs_task_id , ecs_cluster2: $ecs_cluster"
+cat <<EOF> /tmp/fluentbit-tail.conf
+[SERVICE]
+    flush               5
+    daemon              off
+    log_Level           info
+    parsers_file        /fluent-bit/parsers/parsers-app.conf
 
-cat /fluent-bit/etc/fluentbit-tail.conf
+[INPUT]
+    Name                tail
+    Tag                 application
+    Path                $LOG_FILEPATH
+    Parser              logback-json
+    Read_from_Head      False
+    Refresh_Interval    10
+    Rotate_Wait         30
+    Skip_Long_Lines     On
+    Skip_Empty_Lines    On
+
+[OUTPUT]
+    Name                cloudwatch
+    Match               application
+    region              $AWS_REGION
+    log_group_name      /ecs/$ECS_CLUSTER_NAME/$ECS_APP_NAME
+    log_stream_prefix   $ECS_APP_NAME-
+    auto_create_group   true
+    retry_limit         $RETRY_LIMIT
+
+EOF
+
+cp /tmp/fluentbit-tail.conf /fluent-bit/etc/
 
 # see - https://github.com/aws/aws-for-fluent-bit/blob/mainline/entrypoint.sh
 exec /fluent-bit/bin/fluent-bit -e /fluent-bit/firehose.so -e /fluent-bit/cloudwatch.so -e /fluent-bit/kinesis.so -c /fluent-bit/etc/fluentbit-tail.conf
