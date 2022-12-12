0ae1bd4e64bb281fb265613905116d289e0f63e9
symplesims <symplesims@gmail.com>
2022-12-13 01:32:59 +0900

    entrypoint.sh 구동 파일 연결

--- cicd/fluentbit/docker/entrypoint.sh
+++ cicd/fluentbit/docker/entrypoint.sh
@@ -1,13 +1,5 @@
 echo -n "AWS for Fluent Bit Test Image Version "
 cat /AWS_FOR_FLUENT_BIT_VERSION
 
-sed -i "s#{aws_region}#$ECS_AWS_REGION#g" /fluent-bit/etc/fluent-bit.conf
-sed -i "s#{cluster_name}#$ECS_CLUSTER_NAME#g" /fluent-bit/etc/fluent-bit.conf
-sed -i "s#{app_name}#$ECS_APP_NAME#g" /fluent-bit/etc/fluent-bit.conf
-sed -i "s#{log_filepath}#$LOG_FILEPATH#g" /fluent-bit/etc/fluent-bit.conf
-
-# ls -al /var/log/myapp/
-# cat /fluent-bit/etc/fluent-bit.conf
-
 # see - https://github.com/aws/aws-for-fluent-bit/blob/mainline/entrypoint.sh
-exec /fluent-bit/bin/fluent-bit -e /fluent-bit/firehose.so -e /fluent-bit/cloudwatch.so -e /fluent-bit/kinesis.so -c /fluent-bit/etc/fluent-bit.conf
+exec /fluent-bit/bin/fluent-bit -e /fluent-bit/firehose.so -e /fluent-bit/cloudwatch.so -e /fluent-bit/kinesis.so -c /fluent-bit/etc/fluentbit-tail.conf
