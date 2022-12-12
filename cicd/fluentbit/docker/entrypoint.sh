1c040512dab5959160a32978e670d6db8a859981
symplesims <symplesims@gmail.com>
2022-12-13 01:51:20 +0900

    entrypoint.sh 스크립트 로그 출력

--- cicd/fluentbit/docker/entrypoint.sh
+++ cicd/fluentbit/docker/entrypoint.sh
@@ -1,5 +1,10 @@
 echo -n "AWS for Fluent Bit Test Image Version "
 cat /AWS_FOR_FLUENT_BIT_VERSION
 
+echo "ecs_task_id: $(ecs_task_id) ecs_cluster: $(ecs_cluster)"
+echo "ecs_task_id2: $ecs_task_id ecs_cluster2: $ecs_cluster"
+
+cat /fluent-bit/etc/fluentbit-tail.conf
+
 # see - https://github.com/aws/aws-for-fluent-bit/blob/mainline/entrypoint.sh
 exec /fluent-bit/bin/fluent-bit -e /fluent-bit/firehose.so -e /fluent-bit/cloudwatch.so -e /fluent-bit/kinesis.so -c /fluent-bit/etc/fluentbit-tail.conf
